2c27533569386a810440429c02e4410c47629441
imamasafarir <imamsafariramadhan@gmail.com>
2026-09-24 11:56:30 +0800

    Update bug

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,32 @@ use Illuminate\Support\Facades\Route;
 | Shortlink routes → app/Modules/Shortlink/Routes/web.php
 |
 */
+
+// Fallback route untuk Service Worker & PWA Manifest
+Route::get('/sw.js', function () {
+    $path = public_path('sw.js');
+    if (file_exists($path)) {
+        return response()->file($path, [
+            'Content-Type' => 'application/javascript; charset=UTF-8',
+            'Service-Worker-Allowed' => '/',
+        ]);
+    }
+
+    return response('// Service Worker placeholder', 200, [
+        'Content-Type' => 'application/javascript; charset=UTF-8',
+        'Service-Worker-Allowed' => '/',
+    ]);
+});
+
+Route::get('/manifest.json', function () {
+    $path = public_path('manifest.json');
+    if (file_exists($path)) {
+        return response()->file($path, [
+            'Content-Type' => 'application/json; charset=UTF-8',
+        ]);
+    }
+
+    return response('{}', 200, [
+        'Content-Type' => 'application/json; charset=UTF-8',
+    ]);
+});
